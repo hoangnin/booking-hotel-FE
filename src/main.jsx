ccd64aac9befc8c0c1fdf684012f1540643bb1f4
lenin <lenin.vn.hn@gmail.com>
2025-04-21 10:59:18 +0700

    turnoff StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -12,10 +11,8 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
-      {/* Ensure the entire app is wrapped in the Provider */}
-      <App />
-    </Provider>
-  </StrictMode>
+  <Provider store={store}>
+    {/* Ensure the entire app is wrapped in the Provider */}
+    <App />
+  </Provider>
 );
